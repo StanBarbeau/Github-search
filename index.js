05882881487255fedd1cf3e139aed0e4fa745dbf
StanBarbeau <stanislas.barbillon@depinfonancy.net>
2016-09-24 22:23:23 +0200

    24/09/2016 - 22:23
    Première structure

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// On isole et on execute.
+(function (){
+	
+	// ---- initialisation ----
+	// -- Recuperation des variables --
+	
+	var col_result = $('#col-result');
+	var table_body = $('#table_body');
+	var table_line = $('#table_line');
+	var form = $('#form-search');
+	
+	// -- Declaration des fonctions --
+	
+	
+	// ---- MAIN CODE ----
+	// -- gestionnaire d'evement
+	
+	$(form).onsubmit();
+	
+	// -- code additionnel --
+	$(col_result).hide();
+	$(table_line).hide();
+	
+})();
